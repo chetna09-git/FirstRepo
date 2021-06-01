267e79cb3c90450be37108d33cc6b2bb979d93e7
Chetna Gajbhe <chetnagajbhe786@gmail.com>
2021-06-01 11:50:25 +0530

    One example added

---
+++ src/com/capg/Example.java
@@ -0,0 +1,12 @@
+package com.capg;
+
+public class Example {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello Friends Welcome");
+
+	}
+
+}
